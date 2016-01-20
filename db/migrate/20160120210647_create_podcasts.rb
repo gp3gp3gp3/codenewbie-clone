80453e350fd75aea4909b6df1a4db34dae3db0ab
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-01-20 17:28:31 -0500

    Add references to user_model in podcast migration file

--- db/migrate/20160120210647_create_podcasts.rb
+++ db/migrate/20160120210647_create_podcasts.rb
@@ -3,7 +3,7 @@ class CreatePodcasts < ActiveRecord::Migration
     create_table :podcasts do |t|
       t.string :title, null: false
       t.string :description, null: false
-
+      t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
     end
