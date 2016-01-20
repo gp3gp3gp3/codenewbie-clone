039b8a940e3cac5a980b8baf8bd5e72eb03b18c4
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-01-20 17:55:38 -0500

    Remove title from post column in migration file

--- db/migrate/20160120202922_create_posts.rb
+++ db/migrate/20160120202922_create_posts.rb
@@ -1,7 +1,6 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.string :title, null: false
       t.string :body, null: false
       t.references :user, index: true, foreign_key: true
 
