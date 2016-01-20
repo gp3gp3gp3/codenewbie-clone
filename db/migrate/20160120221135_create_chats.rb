229ee545e443f74a4d79dca57809965c9942bb92
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-01-20 17:55:59 -0500

    Remove title from chat migration file

--- db/migrate/20160120221135_create_chats.rb
+++ db/migrate/20160120221135_create_chats.rb
@@ -1,7 +1,6 @@
 class CreateChats < ActiveRecord::Migration
   def change
     create_table :chats do |t|
-      t.string :title, null: false
       t.string :description, null: false
 
       t.timestamps null: false
