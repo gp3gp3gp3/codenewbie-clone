a22df652fecc6c8e76d576fd839cb1fd10765078
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-01-20 16:13:21 -0500

    Add timestamps to posts migration

--- db/migrate/20160120202922_create_posts.rb
+++ db/migrate/20160120202922_create_posts.rb
@@ -4,6 +4,8 @@ class CreatePosts < ActiveRecord::Migration
       t.string :title, null: false
       t.string :body, null: false
       t.references :user, index: true, foreign_key: true
+
+      t.timestamps null: false
     end
   end
 end
