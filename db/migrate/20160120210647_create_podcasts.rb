cdc82ad7d5567f02455683e84626129b615f4792
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-01-20 17:56:14 -0500

    remove title from podcast migration file

--- db/migrate/20160120210647_create_podcasts.rb
+++ db/migrate/20160120210647_create_podcasts.rb
@@ -1,7 +1,6 @@
 class CreatePodcasts < ActiveRecord::Migration
   def change
     create_table :podcasts do |t|
-      t.string :title, null: false
       t.string :description, null: false
       t.references :user, index: true, foreign_key: true
 
