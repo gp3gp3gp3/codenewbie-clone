6d3c719a3b0a127050d2548b428bec49e94ac4cc
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-01-20 16:40:35 -0500

    Add polymorphic linkable reference on link migration file

--- db/migrate/20160120211144_create_links.rb
+++ db/migrate/20160120211144_create_links.rb
@@ -3,6 +3,7 @@ class CreateLinks < ActiveRecord::Migration
     create_table :links do |t|
       t.string :title
       t.string :url, null: false
+      t.references :linkable, polymorphic: true, index: true
 
       t.timestamps null: false
     end
