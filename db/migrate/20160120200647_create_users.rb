2a552d47faca06ea86813cc2b0b0515085359006
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-01-20 17:09:06 -0500

    Add null constraint to name in user migration file

--- db/migrate/20160120200647_create_users.rb
+++ db/migrate/20160120200647_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :full_name
+      t.string :full_name, null: false
       t.string :email, null: false
       t.string :twitter_handle
       t.string :avatar_url
