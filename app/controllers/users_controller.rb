b74a2404cf01d53d8a833b78aff8160ce01dd366
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-01-21 14:23:25 -0500

    Add edit and update user in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,19 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
 
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    user = User.find_by(id: params[:id])
+    if user.update_attributes(user_params)
+      redirect_to user
+    else
+      redirect_to edit_user_path(user)
+    end
+  end
+
   private
 
   def user_params
