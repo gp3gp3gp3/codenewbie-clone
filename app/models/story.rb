1e12f0a865873a86c4c1168f307095fcf45b78d0
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-01-21 15:49:36 -0500

    Add self method to return recent stories

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,7 @@
 class Story < ActiveRecord::Base
   belongs_to :postable, polymorphic: true
 
+  def self.recent_stories(limit)
+    self.order('created_at DESC').limit(limit).map(&:postable)
+  end
 end
