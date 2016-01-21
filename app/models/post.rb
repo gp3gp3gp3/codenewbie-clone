269ad4c99d4a495e2db511bea548328b5750d485
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-01-21 15:49:21 -0500

    Add after create hook on post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
-class Post < Story
+class Post < ActiveRecord::Base
+  belongs_to :author, foreign_key: :user_id
 
+  after_create :create_stories
+
+  private
+
+  def create_stories
+    Story.create(postable: self)
+  end
 
 end
