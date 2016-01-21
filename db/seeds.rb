f17ffdead18286460c60d92c1dbb41ab5dc53896
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-01-21 15:50:55 -0500

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+george = User.create(full_name: "George Paul", email: "george.allen.paul.3@gmail.com", password: "foobar")
+
+george.posts << Post.create(title: Faker::Book.title, body: Faker::Lorem.paragraph)
+george.podcasts << Podcast.create(title: Faker::Hipster.sentence, description: Faker::Hipster.paragraph)
+Chat.create(title: Faker::Book.title, description: Faker::Lorem.paragraph)
+george.podcasts << Podcast.create(title: Faker::Hipster.sentence, description: Faker::Hipster.paragraph)
+Chat.create(title: Faker::Book.title, description: Faker::Lorem.paragraph)
+george.posts << Post.create(title: Faker::Book.title, body: Faker::Lorem.paragraph)
+george.posts << Post.create(title: Faker::Book.title, body: Faker::Lorem.paragraph)
+george.podcasts << Podcast.create(title: Faker::Hipster.sentence, description: Faker::Hipster.paragraph)
+george.posts << Post.create(title: Faker::Book.title, body: Faker::Lorem.paragraph)
+george.podcasts << Podcast.create(title: Faker::Hipster.sentence, description: Faker::Hipster.paragraph)
+Chat.create(title: Faker::Book.title, description: Faker::Lorem.paragraph)
+george.podcasts << Podcast.create(title: Faker::Hipster.sentence, description: Faker::Hipster.paragraph)
+Chat.create(title: Faker::Book.title, description: Faker::Lorem.paragraph)
+george.posts << Post.create(title: Faker::Book.title, body: Faker::Lorem.paragraph)
+george.posts << Post.create(title: Faker::Book.title, body: Faker::Lorem.paragraph)
+george.podcasts << Podcast.create(title: Faker::Hipster.sentence, description: Faker::Hipster.paragraph)
